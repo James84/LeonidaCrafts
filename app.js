19c40ce8fb40c381c6fee1455a6f0612627f3985
James Runham <james.runham@reedonline.co.uk>
2017-02-27 11:20:25 +0000

    set up for keystonejs

--- app.js
+++ app.js
@@ -1,44 +1,95 @@
+//var path = require('path'),
+//    express = require('express'),
+//    expressHandleBars = require('express-handlebars'),
+//    keystone = require('keystone'),
+//    port = process.env.PORT || 8000,
+//    app = express();
+//
+//keystone.set('app', app);
+//
+//// set public folder as static so styles and scripts are available externally
+//app.use(express.static('public'));
+//
+//app.engine('.hbs', expressHandleBars({
+//    defaultLayout: 'main',
+//    extname: '.hbs',
+//    layoutDir: path.join(__dirname, 'templates/views/layouts'),
+//    helpers: {
+//     // set up sections
+//      section: function(name, options){
+//        if(!this._sections){
+//          this._sections = {};
+//        }
+//        this._sections[name] = options.fn(this);
+//        return null;
+//      }
+//    }
+//}));
+//  
+//app.set('view engine', '.hbs');
+//app.set('views', path.join(__dirname, 'views'));
+//
+//app.get('/', function(req, res){
+//    //render homepage
+//    res.render('home', {
+//        name: 'James'
+//    }); 
+//});
+//
+//app.get('/gallery', function(req, res){
+//    res.render('gallery');
+//});
+//
+//app.listen(port, function(err){
+//    console.log('server listening on server ' + port); 
+//});
+//
+//require('./models');
+// 
+//keystone.set('routes', require('./routes'));
+//
+//keystone.start();
+
 var path = require('path');
-var express = require('express');
 var expressHandleBars = require('express-handlebars');
+var keystone = require('keystone');
 
-var app = express();
-
-var port = process.env.PORT || 8000;
-
-// set public folder as static so styles and scripts are available externally
-app.use(express.static('public'));
-
-app.engine('.hbs', expressHandleBars({
-    defaultLayout: 'main',
-    extname: '.hbs',
-    layoutDir: path.join(__dirname, 'views/layouts'),
-    helpers: {
-     // set up sections
-      section: function(name, options){
-        if(!this._sections){
-          this._sections = {};
-        }
-        this._sections[name] = options.fn(this);
-        return null;
-      }
-    }
-}));
+keystone.init({
+    'name': 'LeonidaCrafts',
+    'scss': 'public',
+    'static': ['public'],
+    'favicon': 'public/favicon.ico',
+    'views': 'templates/views',
+    
+    'custom engine': expressHandleBars({
+                        defaultLayout: 'main',
+                        extname: '.hbs',
+                        layoutDir: path.join(__dirname, 'templates/views/layouts'),
+                        helpers: {
+                         // set up sections
+                          section: function(name, options){
+                            if(!this._sections){
+                              this._sections = {};
+                            }
+                            this._sections[name] = options.fn(this);
+                            return null;
+                          }
+                        }
+                    }),
+    
+    'view engine': 'handlebars',
+    
+    'auto update': true,
+    'mongo': 'mongodb://localhost/leonida-crafts',
   
-app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.get('/', function(req, res){
-    //render homepage
-    res.render('home', {
-        name: 'James'
-    }); 
+    'session': true,
+    'auth': true,
+    'user model': 'User',
+    'cookie secret': '93eb055c82b64ad98177a5def4929593'
 });
 
-app.get('/gallery', function(req, res){
-    res.render('gallery');
-});
-
-app.listen(port, function(err){
-    console.log('server listening on server ' + port); 
-});
+require('./models');
+ 
+keystone.set('routes', require('./routes'));
+ 
+keystone.start();
