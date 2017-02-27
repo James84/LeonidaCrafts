fd276321b899f644fa382c2fb324505dd6a378c3
James Runham <james.runham@reedonline.co.uk>
2017-02-27 11:24:49 +0000

    keystone set up

---
+++ updates/0.0.1-admin.js
@@ -0,0 +1,15 @@
+var keystone = require('keystone');
+    User = keystone.List('User');
+
+exports = module.exports = function(next){
+    
+    new User.model({
+        name: {
+            first: 'Admin',
+            last: 'User'
+        },
+        email: 'admin@keystonejs',
+        passwword: 'admin',
+        canAccessKeystone: true
+    }).save(next);
+};
